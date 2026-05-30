546a68592ee3a2d2d3c89814eb0983742bae0c02
Chinemerem Clarence Madu <Chinemerem.Madu@proton.me>
2026-05-30 21:06:21 +0100

    fix: allowed gateway pass

--- backend/auth_api/core/settings.py
+++ backend/auth_api/core/settings.py
@@ -12,7 +12,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[".railway.app", "localhost", "127.0.0.1", "*"])
+ALLOWED_HOSTS = ["*"]
 
 # ====================== INSTALLED APPS ======================
 INSTALLED_APPS = [
